e5269b5b2758e792b0e211540f6af333192daf8e
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 11:33:46 +0700

    membuat class turunan dari class turunan lain

--- Multiwaris/Multiwaris.cpp
+++ Multiwaris/Multiwaris.cpp
@@ -33,9 +33,20 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur):
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur)        // hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
