659404fc91e3af0864def86b0f8917ed49208c5d
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 10:19:47 +0700

    membuat class turunan joko dari class seseorang

--- polymorph/polymorph.cpp
+++ polymorph/polymorph.cpp
@@ -6,13 +6,25 @@ using namespace std;
 class seseorang
 {
 public:
+    //pure virtual function
     virtual void pesan() = 0;
-    //virtual void pesan()
-    // {
-    //      cout << "Pesan Dari Seseorang" << endl;
-    // }
+    //virtual function biasa
+    /*virtual void pesan()
+     {
+          cout << "Pesan Dari Seseorang" << endl;
+     }*/
 };
 
+class joko : public seseorang
+{
+public:
+    //deklarasi
+    void pesan() 
+    {
+        //implementasi
+       cout << "Pesan Dari Joko" << endl;
+    }
+};
 
 int main()
 {
