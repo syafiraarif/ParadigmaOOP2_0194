59e759c4441f18231cbff41917fe19d8c602e8e4
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 11:09:56 +0700

    membuat class AbstraksiKLas

---
+++ Abstraksi/Abstraksi.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas
+{
+private:
+    string x, y;
+
+public:
+
+    //methode untuk mengisi nilai
+    //private number
+
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+
+    void display()
+    {
+        cout << "x = " << x << endl;
+    }
+};
+
+
+int main()
+{
+    
+}
