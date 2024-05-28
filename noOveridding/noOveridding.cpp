bd0f0d4ff616fe69909a4e3d73eee8edd8c60cf5
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 10:32:03 +0700

    membuat class baseclass

---
+++ noOveridding/noOveridding.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahan final sesudah nama class
+//untuk mencegah adanya overriding
+
+class baseclass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Halo saya Function dari base class";
+    }
+};
+
+
+int main()
+{
+    
+}
+
