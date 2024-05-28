7160c4231df9b8ae6b1fedf25f5867aa7a87a90a
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 10:42:32 +0700

    membuat class baseclass dengan akses modifier public

---
+++ noPewarisan/noPewarisan.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+class baseclass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Halo saya Function dari base class";
+    }
+};
+
+int main()
+{
+    
+}
+
