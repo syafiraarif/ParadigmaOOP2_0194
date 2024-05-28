5714c0612c8eea43d1b61a2b07fdec1a57a277a2
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 10:56:25 +0700

    membuat program utama

--- Enkapsulasi/Enkapsulasi.cpp
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -22,6 +22,16 @@ public:
 
 int main()
 {
-    
+    remoteLampu lampuRumah;
+
+    lampuRumah.setsaklarNo(0, "Lampu  Teras Rumah");
+    lampuRumah.setsaklarNo(1, "Lampu  Ruang Tamu");
+    lampuRumah.setsaklarNo(2, "Lampu  Kamar Tidur");
+    lampuRumah.setsaklarNo(3, "Lampu  Dapur");
+
+    cout << lampuRumah.getsaklarNo(0) << endl;
+    cout << lampuRumah.getsaklarNo(1) << endl;
+    cout << lampuRumah.getsaklarNo(2) << endl;
+    cout << lampuRumah.getsaklarNo(3) << endl;
 }
 
