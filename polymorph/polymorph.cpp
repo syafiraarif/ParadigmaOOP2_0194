d044807beb81bb580ad0daacf3ce74921053c292
syairaarif <putrisyafira4444@gmail.com>
2024-05-28 10:12:19 +0700

    Add project files.

---
+++ polymorph/polymorph.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    virtual void pesan() = 0;
+    //virtual void pesan()
+    // {
+    //      cout << "Pesan Dari Seseorang" << endl;
+    // }
+};
+
+
+int main()
+{
+    
+}
+
